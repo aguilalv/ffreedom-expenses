a7f2d27a64f6489bafec84ec0a1463e3db7503ee
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2025-02-03 11:05:42 +0000

    Changed behave setup to get the base url parameter from the call with the right case

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ import os
 
 def before_all(context):
     # 1) Check if user provided a base_url via the -D option
-    base_url = context.config.userdata.get("base_url")
+    base_url = context.config.userdata.get("BASE_URL")
     # 2) If not provided, fall back to an environment variable
     #    or a default localhost
     if not base_url:
